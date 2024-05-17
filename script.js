14f4e397b4b78bd008b6ea9efa0912cc3dce6d6e
LRamires <136740343+LRamires@users.noreply.github.com>
2024-05-17 09:30:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,15 @@
 const products = [
     { id: 1, name: 'Product 1', price: 10 },
     { id: 2, name: 'Product 2', price: 20 },
+    { id: 3, name: 'Product 3', price: 25 },
+    { id: 4, name: 'Product 4', price: 40 },
+    { id: 5, name: 'Product 5', price: 52 },
+    { id: 6, name: 'Product 6', price: 70 },
+    { id: 7, name: 'Product 7', price: 22 },
+    { id: 8, name: 'Product 8', price: 4 },
+    { id: 9, name: 'Product 9', price: 7 },
+    { id: 10, name: 'Product 10', price: 80 },
+    { id: 11, name: 'Product 11', price: 220 },
     // Add more products here
 ];
 
